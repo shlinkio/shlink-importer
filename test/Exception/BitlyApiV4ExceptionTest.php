4a376d7eb94fea5fc62cc6ac6a2933f8af46637c
Alejandro Celaya <alejandrocelaya@gmail.com>
2020-10-22 10:08:59 +0200

    Added continue_token question to BitlyApiV4ParamsConsoleHelper

--- test/Exception/BitlyApiV4ExceptionTest.php
+++ test/Exception/BitlyApiV4ExceptionTest.php
@@ -9,13 +9,24 @@ use Shlinkio\Shlink\Importer\Exception\BitlyApiV4Exception;
 
 class BitlyApiV4ExceptionTest extends TestCase
 {
-    /** @test */
-    public function generatesExpectedMessage(): void
+    /**
+     * @test
+     * @dataProvider provideContinueToken
+     */
+    public function generatesExpectedMessage(?string $continueToken): void
     {
-        $e = BitlyApiV4Exception::fromInvalidRequest('something.com', 500, 'Error body');
+        $e = BitlyApiV4Exception::fromInvalidRequest('something.com', 500, 'Error body', $continueToken);
+
         self::assertEquals(
             'Request to Bitly API v4 to URL "something.com" failed with status code "500" and body "Error body"',
             $e->getMessage(),
         );
+        self::assertEquals($continueToken, $e->continueToken());
+    }
+
+    public function provideContinueToken(): iterable
+    {
+        yield 'no token' => [null];
+        yield 'some token' => ['foobar'];
     }
 }
--- test/Params/ConsoleHelper/BitlyApiV4ParamsConsoleHelperTest.php
+++ test/Params/ConsoleHelper/BitlyApiV4ParamsConsoleHelperTest.php
@@ -11,6 +11,8 @@ use Prophecy\Prophecy\ObjectProphecy;
 use Shlinkio\Shlink\Importer\Params\ConsoleHelper\BitlyApiV4ParamsConsoleHelper;
 use Symfony\Component\Console\Style\StyleInterface;
 
+use function count;
+
 class BitlyApiV4ParamsConsoleHelperTest extends TestCase
 {
     use ProphecyTrait;
@@ -28,62 +30,68 @@ class BitlyApiV4ParamsConsoleHelperTest extends TestCase
      * @test
      * @dataProvider provideResponses
      */
-    public function generatesExpectedParams(string $accessToken, array $responses, array $expected): void
+    public function generatesExpectedParams(array $askResponses, array $confirmResponses, array $expected): void
     {
-        $this->io->ask(Argument::cetera())->willReturn($accessToken);
-        $this->io->confirm(Argument::cetera())->willReturn(...$responses);
+        $ask = $this->io->ask(Argument::cetera())->willReturn(...$askResponses);
+        $confirm = $this->io->confirm(Argument::cetera())->willReturn(...$confirmResponses);
 
         self::assertEquals($expected, $this->paramsHelper->requestParams($this->io->reveal()));
+        $ask->shouldHaveBeenCalledTimes(count($askResponses));
+        $confirm->shouldHaveBeenCalledTimes(count($confirmResponses));
     }
 
     public function provideResponses(): iterable
     {
         yield [
-            $accessToken = 'foobar',
+            ['foobar', null],
             [true, true, true, true, true],
             [
-                'access_token' => $accessToken,
+                'access_token' => 'foobar',
                 'import_short_codes' => true,
                 'import_tags' => true,
                 'import_custom_domains' => true,
                 'keep_creation_date' => true,
                 'ignore_archived' => true,
+                'continue_token' => null,
             ],
         ];
         yield [
-            $accessToken = 'barfoo',
+            ['barfoo', null],
             [false, false, false, false, false],
             [
-                'access_token' => $accessToken,
+                'access_token' => 'barfoo',
                 'import_short_codes' => false,
                 'import_tags' => false,
                 'import_custom_domains' => false,
                 'keep_creation_date' => false,
                 'ignore_archived' => false,
+                'continue_token' => null,
             ],
         ];
         yield [
-            $accessToken = 'accesstoken',
+            ['accesstoken', 'continue_from_here'],
             [false, true, false, true, false],
             [
-                'access_token' => $accessToken,
+                'access_token' => 'accesstoken',
                 'import_short_codes' => false,
                 'import_tags' => true,
                 'import_custom_domains' => false,
                 'keep_creation_date' => true,
                 'ignore_archived' => false,
+                'continue_token' => 'continue_from_here',
             ],
         ];
         yield [
-            $accessToken = 'something',
+            ['something', 'token'],
             [true, true, true, false, false],
             [
-                'access_token' => $accessToken,
+                'access_token' => 'something',
                 'import_short_codes' => true,
                 'import_tags' => true,
                 'import_custom_domains' => true,
                 'keep_creation_date' => false,
                 'ignore_archived' => false,
+                'continue_token' => 'token',
             ],
         ];
     }
