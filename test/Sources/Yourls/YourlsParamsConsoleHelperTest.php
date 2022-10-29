3b1b8935be60c0a6ffa9c1dc94076b5d89e7e114
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-29 12:46:13 +0200

    Updated YourlsParamsConsoleHelperTest to use PHPUnit mocks intead of prophecy

--- test/Sources/Yourls/YourlsParamsConsoleHelperTest.php
+++ test/Sources/Yourls/YourlsParamsConsoleHelperTest.php
@@ -4,38 +4,42 @@ declare(strict_types=1);
 
 namespace ShlinkioTest\Shlink\Importer\Sources\Yourls;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Shlinkio\Shlink\Importer\Params\ParamsUtils;
 use Shlinkio\Shlink\Importer\Sources\Yourls\YourlsParamsConsoleHelper;
 use Symfony\Component\Console\Style\StyleInterface;
 
 class YourlsParamsConsoleHelperTest extends TestCase
 {
-    use ProphecyTrait;
-
     private YourlsParamsConsoleHelper $helper;
-    private ObjectProphecy $io;
+    private MockObject & StyleInterface $io;
 
     public function setUp(): void
     {
         $this->helper = new YourlsParamsConsoleHelper();
-        $this->io = $this->prophesize(StyleInterface::class);
+        $this->io = $this->createMock(StyleInterface::class);
     }
 
     /** @test */
     public function expectedQuestionsAreAsked(): void
     {
-        $askBaseUrl = $this->io->ask('What is your YOURLS instance base URL?')->willReturn('foo.com');
-        $askUser = $this->io->ask('What is your YOURLS instance username?')->willReturn('user');
-        $askPassword = $this->io->ask('What is your YOURLS instance password?')->willReturn('pass');
-        $askDomain = $this->io->ask(
-            'To what domain do you want the URLs to be linked? (leave empty to link them to default domain)',
-        )->willReturn('domain');
-        $importVisits = $this->io->confirm('Do you want to import each short URL\'s visits too?')->willReturn(true);
+        $this->io->expects($this->exactly(4))->method('ask')->willReturnMap([
+            ['What is your YOURLS instance base URL?', null, null, 'foo.com'],
+            ['What is your YOURLS instance username?', null, null, 'user'],
+            ['What is your YOURLS instance password?', null, null, 'pass'],
+            [
+                'To what domain do you want the URLs to be linked? (leave empty to link them to default domain)',
+                null,
+                null,
+                'domain',
+            ],
+        ]);
+        $this->io->expects($this->once())->method('confirm')->with(
+            'Do you want to import each short URL\'s visits too?',
+        )->willReturn(true);
 
-        $result = ParamsUtils::invokeCallbacks($this->helper->requestParams($this->io->reveal()));
+        $result = ParamsUtils::invokeCallbacks($this->helper->requestParams($this->io));
 
         self::assertEquals([
             'base_url' => 'foo.com',
@@ -44,10 +48,5 @@ class YourlsParamsConsoleHelperTest extends TestCase
             'import_visits' => true,
             'domain' => 'domain',
         ], $result);
-        $askBaseUrl->shouldHaveBeenCalledOnce();
-        $askUser->shouldHaveBeenCalledOnce();
-        $askPassword->shouldHaveBeenCalledOnce();
-        $askDomain->shouldHaveBeenCalledOnce();
-        $importVisits->shouldHaveBeenCalledOnce();
     }
 }
