5cba1468d712f28a94a6166500783f6f91008136
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-29 12:17:02 +0200

    Updated RestApiConsumerTest to use PHPUnit mocks intead of prophecy

--- test/Http/RestApiConsumerTest.php
+++ test/Http/RestApiConsumerTest.php
@@ -8,10 +8,8 @@ use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use JsonException;
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
@@ -20,18 +18,16 @@ use Shlinkio\Shlink\Importer\Http\RestApiConsumer;
 
 class RestApiConsumerTest extends TestCase
 {
-    use ProphecyTrait;
-
     private RestApiConsumer $apiConsumer;
-    private ObjectProphecy $httpClient;
-    private ObjectProphecy $requestFactory;
+    private MockObject & ClientInterface $httpClient;
+    private MockObject & RequestFactoryInterface $requestFactory;
 
     public function setUp(): void
     {
-        $this->httpClient = $this->prophesize(ClientInterface::class);
-        $this->requestFactory = $this->prophesize(RequestFactoryInterface::class);
+        $this->httpClient = $this->createMock(ClientInterface::class);
+        $this->requestFactory = $this->createMock(RequestFactoryInterface::class);
 
-        $this->apiConsumer = new RestApiConsumer($this->httpClient->reveal(), $this->requestFactory->reveal());
+        $this->apiConsumer = new RestApiConsumer($this->httpClient, $this->requestFactory);
     }
 
     /**
@@ -41,13 +37,11 @@ class RestApiConsumerTest extends TestCase
     public function exceptionIsThrownWhenRequestReturnsNonSuccessfulResponse(int $status): void
     {
         $req = new Request('GET', '');
-        $createRequest = $this->requestFactory->createRequest(Argument::cetera())->willReturn($req);
-        $sendRequest = $this->httpClient->sendRequest($req)->willReturn(new Response($status));
+        $this->requestFactory->expects($this->once())->method('createRequest')->willReturn($req);
+        $this->httpClient->expects($this->once())->method('sendRequest')->with($req)->willReturn(new Response($status));
 
         $this->expectException(InvalidRequestException::class);
         $this->expectDeprecationMessage('Request to /foo/bar failed with status code ' . $status);
-        $createRequest->shouldBeCalledOnce();
-        $sendRequest->shouldBeCalledOnce();
 
         $this->apiConsumer->callApi('/foo/bar');
     }
@@ -67,12 +61,12 @@ class RestApiConsumerTest extends TestCase
     public function exceptionIsThrownIfResponseBodyIsNotValidJson(): void
     {
         $req = new Request('GET', '');
-        $createRequest = $this->requestFactory->createRequest(Argument::cetera())->willReturn($req);
-        $sendRequest = $this->httpClient->sendRequest($req)->willReturn(new Response(200, [], '{"foo'));
+        $this->requestFactory->expects($this->once())->method('createRequest')->willReturn($req);
+        $this->httpClient->expects($this->once())->method('sendRequest')->with($req)->willReturn(
+            new Response(200, [], '{"foo'),
+        );
 
         $this->expectException(JsonException::class);
-        $createRequest->shouldBeCalledOnce();
-        $sendRequest->shouldBeCalledOnce();
 
         $this->apiConsumer->callApi('/foo/bar');
     }
@@ -81,17 +75,19 @@ class RestApiConsumerTest extends TestCase
     public function responseBodyIsParsedWhenProperJsonIsReturned(): void
     {
         $req = new Request('GET', '');
-        $createRequest = $this->requestFactory->createRequest(Argument::cetera())->willReturn($req);
-        $sendRequest = $this->httpClient->sendRequest(Argument::that(function (RequestInterface $request): bool {
-            $headers = $request->getHeaders();
+        $this->requestFactory->expects($this->once())->method('createRequest')->willReturn($req);
+        $this->httpClient->expects($this->once())->method('sendRequest')->with($this->callback(
+            function (RequestInterface $request): bool {
+                $headers = $request->getHeaders();
 
-            Assert::assertArrayHasKey('Authorization', $headers);
-            Assert::assertEquals('Bearer foobar', $request->getHeaderLine('Authorization'));
-            Assert::assertArrayHasKey('X-Api-Key', $headers);
-            Assert::assertEquals('abc-123', $request->getHeaderLine('X-Api-Key'));
+                Assert::assertArrayHasKey('Authorization', $headers);
+                Assert::assertEquals('Bearer foobar', $request->getHeaderLine('Authorization'));
+                Assert::assertArrayHasKey('X-Api-Key', $headers);
+                Assert::assertEquals('abc-123', $request->getHeaderLine('X-Api-Key'));
 
-            return true;
-        }))->willReturn(new Response(200, [], '{"foo": "bar"}'));
+                return true;
+            },
+        ))->willReturn(new Response(200, [], '{"foo": "bar"}'));
 
         $result = $this->apiConsumer->callApi('/foo/bar', [
             'Authorization' => 'Bearer foobar',
@@ -99,7 +95,5 @@ class RestApiConsumerTest extends TestCase
         ]);
 
         self::assertEquals(['foo' => 'bar'], $result);
-        $createRequest->shouldHaveBeenCalledOnce();
-        $sendRequest->shouldHaveBeenCalledOnce();
     }
 }
