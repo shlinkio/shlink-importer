b036da25b9208e600bbe6fbef36a39325aa91baa
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-07-22 10:05:16 +0200

    Update dependencies

--- src/Params/ConsoleHelper/ConsoleHelperManager.php
+++ src/Params/ConsoleHelper/ConsoleHelperManager.php
@@ -6,7 +6,11 @@ namespace Shlinkio\Shlink\Importer\Params\ConsoleHelper;
 
 use Laminas\ServiceManager\AbstractPluginManager;
 
+/**
+ * @extends AbstractPluginManager<ParamsConsoleHelperInterface>
+ */
 class ConsoleHelperManager extends AbstractPluginManager implements ConsoleHelperManagerInterface
 {
+    /** @var class-string<ParamsConsoleHelperInterface> */
     protected $instanceOf = ParamsConsoleHelperInterface::class; // phpcs:ignore
 }
--- src/Strategy/ImporterStrategyManager.php
+++ src/Strategy/ImporterStrategyManager.php
@@ -6,7 +6,11 @@ namespace Shlinkio\Shlink\Importer\Strategy;
 
 use Laminas\ServiceManager\AbstractPluginManager;
 
+/**
+ * @extends AbstractPluginManager<ImporterStrategyInterface>
+ */
 class ImporterStrategyManager extends AbstractPluginManager implements ImporterStrategyManagerInterface
 {
+    /** @var class-string<ImporterStrategyInterface> */
     protected $instanceOf = ImporterStrategyInterface::class; // phpcs:ignore
 }
